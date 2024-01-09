dfd0679a29f7121861462cad2b72f37ac2ca8669
Alejandro Vicinay Estrade <alvicina@c4r3s6.42urduliz.com>
2024-01-09 12:41:24 +0100

    se añade nota explicativa del modulo

--- mandatory/builtin_echo.c
+++ mandatory/builtin_echo.c
@@ -6,11 +6,25 @@
 /*   By: alvicina <alvicina@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:38:10 by alvicina          #+#    #+#             */
-/*   Updated: 2024/01/09 12:29:59 by alvicina         ###   ########.fr       */
+/*   Updated: 2024/01/09 12:40:11 by alvicina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+// NOTA: Este modulo no aloja memoria dinamica. Su INPUT es un puntero a un
+// array de strings con el los argumentos para el modulo:
+
+//  char *cmd[5];
+// 	cmd[0] = "echo";
+//	cmd[1] = "pedro";
+//	cmd[2] = "aita";
+//	cmd[3] = "pepe";
+//	cmd[4] = NULL;
+
+// NOTA: Para este proyecto solo tenemos que replicar la opcion "-n", que
+// elimina el salto de linea final. Esta opcion siempre se tiene que
+// introducir por el usuario como segundo argumento (argv[1]);
 
 static int	ft_check_option(char *option)
 {
