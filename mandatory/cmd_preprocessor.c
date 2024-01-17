d9f11f8fcba7c7bbb8dcd074d3fccca219e66795
Alejandro Vicinay Estrade <alvicina@c3r4s6.42urduliz.com>
2024-01-17 11:31:40 +0100

    se añade que reemplaze no solo espacio por tab si no saltos de line, tab vertical, salto de pagina, retorno de carro

--- mandatory/cmd_preprocessor.c
+++ mandatory/cmd_preprocessor.c
@@ -3,44 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_preprocessor.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afidalgo <afidalgo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alvicina <alvicina@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 10:26:09 by afidalgo          #+#    #+#             */
-/*   Updated: 2024/01/08 19:45:44 by afidalgo         ###   ########.fr       */
+/*   Updated: 2024/01/17 11:09:46 by alvicina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static char	*replace_chars(char *str, char find, char replace);
-
-// TODO: Esta función deberia devolver la linea recibida por parámetro lista para ser parseada al AST,
-// TODO: podria devolver un puntero null en caso de que el comando no sea valido.
-char	**preprocess(char *line)
+static char	*replace_chars(char *str, char *to_find, char replace)
 {
-	// TODO: Sustituir variables de entorno por su valor.
+	size_t	i;
+	size_t	j;
 
-	// TODO: No interpretar caracteres especiales on especificados en el subject.
-	// TODO: No estoy seguro de si deberiamos eliminar estos caracteres 
-	// TODO: y continuar con la ejecución o devolver un error en caso de encontrar alguno.
-	
+	i = 0;
+	while (str[i])
+	{	
+		j = 0;
+		while (to_find[j])
+		{
+			if (str[i] == to_find[j])
+				str[i] = replace;
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
+
+char	**preprocess(char *line)
+{	
 	char	**ret;
+	char	*to_find;
 
-	line = replace_chars(line, '\t', ' ');
+	to_find = "\t\n\v\f\r";
+	line = replace_chars(line, to_find, ' ');
 	ret = ft_split(line, ' ');
 	return (ret);
 }
 
-static char	*replace_chars(char *str, char find, char replace)
+int	main(void)
 {
-	int	i;
+	//char	line[100] = "hola \t\v\f\r que\t\n\v\f\rtal ";
+	char	*line;
+	char	**line_process;
+	size_t	i;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == find)
-			str[i] = replace;
-		i++;
-	}
-	return (str);
+	//while (1)
+	//{
+		line = readline("minishell>");
+		line_process = preprocess(line);
+		i = 0;
+		while (line_process[i])
+		{
+			printf("%s\n", line_process[i]);
+			i++;
+		}
+		ft_free_env(line_process);
+		free(line);
+	//}
+	return (0);
 }
