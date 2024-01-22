ac58f351a02cbe2168b289239f3f8463db142209
Alejandro Vicinay Estrade <alvicina@c3r4s3.42urduliz.com>
2024-01-22 13:30:19 +0100

    identificados que string hay que expandir y cuales no

--- mandatory/cmd_expand.c
+++ mandatory/cmd_expand.c
@@ -6,17 +6,64 @@
 /*   By: alvicina <alvicina@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 10:46:05 by alvicina          #+#    #+#             */
-/*   Updated: 2024/01/22 10:54:31 by alvicina         ###   ########.fr       */
+/*   Updated: 2024/01/22 13:29:21 by alvicina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static int	exec_expand(char *ret)
+{
+	
+}
+
+static int	check_dollar(char *ret)
+{
+	size_t	i;
+
+	i = 0;
+	while(ret[i])
+	{
+		if (ret[i] == '$')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	check_expand(char *ret)
+{
+	size_t	i;
+
+	i = 0;
+	while (ret[i])
+	{
+		if (ret[i] == '\'')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	do_expand(char **ret)
 {
 	size_t	i;
 	
 	i = 0;
-	(void) ret;
+	while (ret[i])
+	{
+		if (check_expand(ret[i]))
+		{
+			if(check_dollar(ret[i]))
+			{
+				printf("******tiene DOLLAR*******%s\n", ret[i]);
+				exec_expand(ret[i]);
+			}
+		}
+		i++;	
+	}
 	return (0);
 }
+
+
+/*ls -l | grep \'hola $que tal\' $aqui \'estoy 1adios \"   pepe \"   adios\' 2\'ho\'la \"que $tal\"*/
